08344057383f7024010333bfd3a75ac7cc054999
Stefan Sundström <mixedbystefan@gmail.com>
2019-03-05 13:36:10 +0100

    Mindre Justering

--- CalculatorTDD/src/calculator/Calculator.java
+++ CalculatorTDD/src/calculator/Calculator.java
@@ -25,17 +25,14 @@ public class Calculator {
 		Scanner scanner =new Scanner(System.in);
 		String input = scanner.nextLine();
 		Calculator calculator = new Calculator();
-		String output = calculator.replaceDoubleMinus(input);
+		String output = calculator.checkParanteses(input);
 		//String output = calculator.calculateExpression(input);
 		System.out.println(output);
 		scanner.close();
 		
 	}
 	
-	public String replaceDoubleMinus(String s) {
-		
-		return s;
-	}
+	
 	
 	public String checkParanteses(String s){             //metod som hanterar paranteser separat
         check = new Calculator();
@@ -51,7 +48,7 @@ public class Calculator {
                     for(int i=o; i>=0;i--){
                         if(s.charAt(i)=='('){                          //söker öppnande parantes
                             String in = s.substring(i+1,o);
-                            in = check.calculateExpressionBrackets(in);
+                            in = check.calculateExpression(in);
                             s=s.substring(0,i)+in+s.substring(o+1);
                             i=o=0;
                         }
@@ -74,189 +71,6 @@ public class Calculator {
 	// anropar en metod beroende på input och returnerar en summa
 	
 	public String calculateExpression(String expression) {
-	
-		
-		double result = 0.0;
-		double mem = 0.0;
-		double mem_2 = 0.0;
-		int index = 0;
-		ArrayList<String> upDatedList = new ArrayList<String>();
-	
-		// Delar upp input i en lista
-		String temp[] = expression.split("(?<=[\\(\\)\\+\\-*\\/\\^A-Za-z])|(?=[\\(\\)\\+\\-*\\/\\^A-Za-z])");
-		
-		
-		
-		if (temp.length >1) 
-		{
-			
-			
-		for (int i=0; i<temp.length; i++)
-		{ 	
-			
-			if (temp[i].equalsIgnoreCase(("+"))|| temp[i].equalsIgnoreCase(("-")))
-			{mem=0.0;}
-			
-			if (temp[i].equalsIgnoreCase(("*"))|| temp[i].equalsIgnoreCase(("/")))
-			{
-				
-				double d1 = Double.parseDouble(temp[i-1]);
-				double d2 = Double.parseDouble(temp[i+1]);
-				if (temp[i].equalsIgnoreCase(("*"))) 
-				{
-					if (mem==0.0) 
-						{
-						result = multiply(d1, d2); mem=result;
-						temp[i-1]="";
-						temp[i]="";
-						temp[i+1]=Double.toString(result);
-						
-						
-						}
-					else 
-						{
-						result = multiply(mem, d2); mem=result;
-						temp[i-1]="";
-						temp[i]="";
-						temp[i+1]=Double.toString(result);
-						}
-				}
-				if (temp[i].equalsIgnoreCase(("/"))) 
-				{
-					
-					if (mem==0.0) 
-						{
-						result = divide(d1, d2); mem=result;
-						temp[i-1]="";
-						temp[i]="";
-						temp[i+1]=Double.toString(result);
-						}
-					else 
-					{
-						result = divide(mem, d2); mem=result;
-						temp[i-1]="";
-						temp[i]="";
-						temp[i+1]=Double.toString(result);
-					}
-				}
-				
-			
-			}
-			
-			
-			
-			// temp ska tömmas på tomma index
-			
-			
-			
-		}
-		
-		StringBuffer sBuffer = new StringBuffer();
-	    
-	    
-		for (int i=0; i<temp.length; i++)
-		{
-			if (!temp[i].equalsIgnoreCase("")) 
-			{upDatedList.add(temp[i]);} 
-			
-			sBuffer.append(temp[i]);
-		
-		}
-		String updatedExpression = sBuffer.toString();
-		String _temp[] = updatedExpression.split("(?<=[\\(\\)\\+\\-*\\/\\^A-Za-z])|(?=[\\(\\)\\+\\-*\\/\\^A-Za-z])");
-		temp = _temp;
-		
-		
-		
-		
-		
-		for (int i=0; i<temp.length; i++)
-		{ 
-		
-		if (temp[i].equalsIgnoreCase(("+"))|| temp[i].equalsIgnoreCase(("-")))
-		{
-			
-			double d1 = Double.parseDouble(temp[i-1]);
-			double d2 = Double.parseDouble(temp[i+1]);
-			
-			if (temp[i].equalsIgnoreCase(("+"))) 
-			{
-				if (mem_2==0.0) {
-					result = add(d1, d2); mem_2=result;
-					}
-				else 
-					{
-					result = add(mem_2, d2); mem_2=result;
-					}
-			}
-		
-			if (temp[i].equalsIgnoreCase(("-"))) 
-			{
-				
-				if (mem_2==0.0) 
-					{
-					result = subtract(d1, d2); mem_2=result;
-					}
-				else 
-				{
-					result = subtract(mem_2, d2); mem_2=result;
-				}
-			}
-			
-		
-		}
-		}
-		
-		}
-		
-		
-		
-		
-		
-		/*split = expression.split("[-]");
-		if (split.length >1) 
-		{ 
-			double d1=0;
-			for (int i=1; i<split.length; i++)
-			{
-				
-				if (i==1) {d1 = Double.parseDouble(split[i-1]);}
-				double d2 = Double.parseDouble(split[i]);
-				result = subtract(d1, d2);
-				d1 = result;
-			}
-			
-			
-			
-			 
-		}
-		
-		split = expression.split("[+]");
-		if (split.length >1) 
-		{ 
-			
-			for (int i=1; i<split.length; i++)
-			{
-				
-				double d1 = Double.parseDouble(split[i-1]);
-				double d2 = Double.parseDouble(split[i+1]);
-				result = add(d1, d2);
-				
-			}
-			
-			
-			
-			 
-		}*/
-		
-		String out = Double.toString(result);
-		System.out.println("detta är final-strängen ut : " + expression);
-		return out;
-	
-	}
-	
-	
-public String calculateExpressionBrackets(String expression) {
 	
 		
 		double result = 0.0;
